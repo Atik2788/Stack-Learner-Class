e5481c0fce8606adada9fda499c57a958e88a06f
Atik2788 <ashanto653.2@gmail.com>
2024-10-26 23:17:39 +0600

    lec 9

--- lec-9.js
+++ lec-9.js
@@ -13,3 +13,11 @@ function formUser(random1, random2, ){
 }
 console.log(generateTowRandomNumber(50, formUser));
 
+// 2nd way ****************
+generateTowRandomNumber(50, (random1, random2) =>{
+    console.log(random1, random2);
+})
+
+console.log(generateTowRandomNumber(20, (ran1, ran2) => ran1 * ran2));
+console.log(generateTowRandomNumber(100, (ran1, ran2) => ran1 - ran2));
+console.log(generateTowRandomNumber(10, (ran1, ran2) => ran1 / ran2));
