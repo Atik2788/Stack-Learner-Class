3afe1d65b3f577cffb6f8c72a38565f8a6d61a5f
Md. Atikur Rahman Shanta <107786516+Atik2788@users.noreply.github.com>
2024-11-26 11:13:00 +0600

    call wait function

--- Lec-10.js
+++ Lec-10.js
@@ -1,1 +1,13 @@
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+wait(2000).then(() =>{
+    console.log('Done in 2000 ms')
+})
+
+wait(3000).then(() =>{
+    console.log('Done in 3000 ms')
+})
+
+wait(5000).then(() =>{
+    console.log('Done in 5000 ms')
+})
