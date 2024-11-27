38cc3b434e2510f15913688d6a2c5647f976e3c5
Md. Atikur Rahman Shanta <107786516+Atik2788@users.noreply.github.com>
2024-11-27 16:42:30 +0600

    Update Lec-10.js get data using axios and get function

--- Lec-10.js
+++ Lec-10.js
@@ -11,3 +11,31 @@ wait(3000).then(() =>{
 wait(5000).then(() =>{
     console.log('Done in 5000 ms')
 })
+
+
+// use in project
+// use in project
+const axios = require("axios");
+//npm i axios
+
+const USERS_URl = 'https://jsonplaceholder.typicode.com/users';
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+const COMMENT_URL = 'https://jsonplaceholder.typicode.com/comments';
+
+// user name = Bret
+
+async function getComment(username){
+try{
+    const {data: user} = await axios.get(`${USERS_URl}?username=${username}`);
+    const {data: posts} = await axios.get(`${POSTS_URL}?userId=${user[0].id}`)
+    const {data: comments} = await axios.get(`${COMMENT_URL}?postId=${posts[0].id}`)
+    const {data: userWithComment} = await axios.get(`${USERS_URl}?email=${comments[0].email}`)
+    
+    console.log('comments user: ', userWithComment)
+} catch(e){
+    console.log("Error finding: ", e.message)
+}
+
+}
+
+getComment('Bret')
