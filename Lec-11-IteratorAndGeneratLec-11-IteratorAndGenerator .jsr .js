b90c315a4c404a3e73a9c31da3466c87e6bfd88b
Md. Atikur Rahman Shanta <107786516+Atik2788@users.noreply.github.com>
2024-11-28 16:50:59 +0600

    adding some more code

--- Lec-11-IteratorAndGeneratLec-11-IteratorAndGenerator .jsr .js
+++ Lec-11-IteratorAndGeneratLec-11-IteratorAndGenerator .jsr .js
@@ -12,7 +12,7 @@ console.log(channelIterator.next())
 console.log(channelIterator.next())
 
 
-/* value
+/* output value
 (2) {value: "H", done: false}
 (2) {value: "e", done: false}
 (2) {value: "l", done: false}
@@ -22,3 +22,20 @@ console.log(channelIterator.next())
 (2) {value: undefined, done: true}
 (2) {value: undefined, done: true}
   */
+
+
+while (true) {
+  const data = channelIterator.next();
+  
+  if(data.done){
+    break
+  }
+  console.log(data)
+}
+
+// output value:
+// (2) {value: "H", done: false}
+// (2) {value: "e", done: false}
+// (2) {value: "l", done: false}
+// (2) {value: "l", done: false}
+// (2) {value: "o", done: false}
